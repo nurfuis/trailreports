372d33e7c8b8903bf6e43df67988ed1284bba160
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-01 14:53:25 -0500

    1

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 $page_title = "Keep Sur Wild";
 $page_css = "./assets/css/style.css";
 
-include ("./components/head.php"); // Top section up to and including body tag
-include ("./layouts/main.php"); // An open div with layout class
+include ("./components/head.inc"); // Top section up to and including body tag
+include ("./layouts/main.inc"); // An open div with layout class
 
-include ("./components/nav.php"); // standalone div
-include ("./components/welcome-header.php"); // standalone div
-include ("./components/topo-map.php"); // standalone div + script
+include ("./components/nav.inc"); // standalone div
+include ("./components/welcome-header.inc"); // standalone div
+include ("./components/topo-map.inc"); // standalone div + script
 
-include ("./components/tail.php"); // closing tags for layout div, body, and html
+include ("./components/tail.inc"); // closing tags for layout div, body, and html
 
 include_once ("../db_connect.php"); // $msqli connect
 // $query = "SELECT * FROM users";
