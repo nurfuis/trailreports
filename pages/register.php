6bf4cb72cb2f8a52feb41e05adf20904da935e3a
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-01 16:28:52 -0500

    1

--- pages/register.php
+++ pages/register.php
@@ -5,7 +5,7 @@ $page_css = "../assets/css/style.css";
 
 include ("../layouts/head.inc"); // Top section up to and including body tag
 include ("../layouts/secondary.inc"); // An open div with layout class
-include ("../components/registration");
+include ("../components/registration-form.inc");
 
 include ("../layouts/tail.inc"); // closing tags for layout div, body, and html
 
@@ -38,16 +38,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// $query = "SELECT * FROM users";
-// $result = mysqli_query($mysqli, $query);
+$query = "SELECT * FROM users";
+$result = mysqli_query($mysqli, $query);
 
-// if ($result) {
-//     $users = mysqli_fetch_all($result);
+if ($result) {
+    $users = mysqli_fetch_all($result);
 
-//     foreach ($users as $user) {
-//         echo '<h6>Username: </h6>', $user[1], ' <h6>Registration date: </h6>', $user[3];
-//     }
+    foreach ($users as $user) {
+        echo '<h6>Username: </h6>', $user[1], ' <h6>Registration date: </h6>', $user[3];
+    }
 
-//     mysqli_free_result($result);
-// }
+    mysqli_free_result($result);
+}
 mysqli_close($mysqli);
