a29aab469a488f89a7ac1f75e83290ecb5b55301
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-01 12:59:22 -0500

    v1

---
+++ www/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseUsername = 'mission_minded_mfer';
+$databasePassword = 'u9UzFjfpKAWs7YCxdLEVmtN';
+$databaseName = 'keep_sur_wild';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
