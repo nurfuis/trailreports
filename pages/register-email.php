11554dd09b40b32744cbfd1f375dc100745148c5
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-02 19:22:21 -0500

    1

--- pages/register-email.php
+++ pages/register-email.php
@@ -28,12 +28,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   echo "Check email availability";
 
   $sql = "SELECT COUNT(*) FROM users WHERE email = ?";
+  echo $sql;
   $stmt = mysqli_prepare($mysqli, $sql);
   mysqli_stmt_bind_param($stmt, "s", $email);
   mysqli_stmt_execute($stmt);
   $result = mysqli_stmt_get_result($stmt);
   $row = mysqli_fetch_assoc($result);
-  echo "result:" . $result;
 
   if ($row['COUNT(*)'] > 0) {
     $errorMessage = "Email already exists. Please use a different email.";
