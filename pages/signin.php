f8d3ff90520bbc36640353ba1b99134a09881001
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-03 16:48:01 -0500

    1

--- pages/signin.php
+++ pages/signin.php
@@ -3,7 +3,16 @@
 $page_title = "Signin";
 $page_css = "/assets/css/style.css";
 
-include ("../components/head.inc"); 
-include ("../layouts/secondary.inc"); 
-include ("../components/signin_form.inc");
+include ("../components/head.inc");
+include ("../layouts/secondary.inc");
+
+session_start();
+if (isset($_SESSION['user_id'])) {
+    $username = $_SESSION['username'];
+    include ("./components/is_logged_in.inc");
+} else {
+    include ("../components/signin_form.inc");
+
+}
+
 include ("../layouts/tail.inc");
