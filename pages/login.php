571c46aa6db85e7b16f4fffba631d8fb6efbd34d
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-03 16:14:39 -0500

    1

--- pages/login.php
+++ pages/login.php
@@ -55,16 +55,6 @@ if (!empty($errorMessage)) {
 
 ?>
 
-<h2>Login</h2>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-  <label for="username">Username:</label>
-  <input type="text" id="username" name="username" required><br><br>
-  <label for="password">Password:</label>
-  <input type="password" id="password" name="password" required><br><br>
-  <input type="submit" value="Login">
-</form>
-
-<p>Don't have an account? <a href="register.php">Register Here</a></p>
 
 <?php include ("../layouts/tail.inc"); // closing tags for layout div, body, and html ?>
