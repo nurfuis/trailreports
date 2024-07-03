9ad4222a6083eb5d5715eb69188a15a65a2ea852
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-03 16:48:35 -0500

    1

--- pages/signin.php
+++ pages/signin.php
@@ -9,7 +9,7 @@ include ("../layouts/secondary.inc");
 session_start();
 if (isset($_SESSION['user_id'])) {
     $username = $_SESSION['username'];
-    include ("./components/is_logged_in.inc");
+    include ("../components/is_logged_in.inc");
 } else {
     include ("../components/signin_form.inc");
 
