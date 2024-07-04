a0d1845a429851ea84d671fcca03d980e5355bb3
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-03 22:17:50 -0500

    1

--- pages/reset_password.php
+++ pages/reset_password.php
@@ -1,14 +1,40 @@
+<?php
+
+$page_title = "Reset Password";
+$page_css = "/assets/css/style.css";
+
+include ("../components/head.inc");
+include ("../layouts/secondary.inc");
+
+require_once ("../../db_connect.php"); // Include database connection
+
+?>
 <h2>Reset Password</h2>
 
 <?php
 
-// Check if a valid token is present in the URL
 if (isset($_GET['token'])) {
   $token = $_GET['token'];
-  echo 'valid'. $token .'';
-  // Validate token and expiry time on server-side (process_reset_password.php)
+
+  $sql = "SELECT user_id, reset_token_expiry FROM users WHERE reset_token = ?";
+  $stmt = mysqli_prepare($mysqli, $sql);
+  mysqli_stmt_bind_param($stmt, "s", $token);
+  mysqli_stmt_execute($stmt);
+
+  $result = mysqli_stmt_get_result($stmt);
+
+  $num_rows = $result->num_rows;  // Store the number of rows
+
+  if ($num_rows === 1) {
+    echo "1";
+  }
+
 } else {
   echo "<p style='color: red;'>Invalid password reset link.</p>";
   exit;
 }
+
+mysqli_close($mysqli);
+
+include ("../layouts/tail.inc");
 ?>
