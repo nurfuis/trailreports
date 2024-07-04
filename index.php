a9bd680642900483623a19ac951035f354aefeb6
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-03 23:32:19 -0500

    1

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 $page_title = "Keep Sur Wild";
 $page_css = "/assets/css/style.css";
 
-include ("./components/head.inc"); // Top section up to and including body tag
-include ("./layouts/main.inc"); // An open div with layout class
+include ("./components/head.inc");
+include ("./layouts/main.inc");
+
+include ("./components/nav.inc");
 
 
-include ("./components/nav.inc"); // standalone div
-include ("./components/welcome_header.inc"); // standalone div
-include ("./components/topo_map.inc"); // standalone div + script
+include ("./components/welcome_header.inc");
+include ("./components/topo_map.inc");
 
 session_start();
 if (isset($_SESSION['user_id'])) {
@@ -19,19 +20,4 @@ if (isset($_SESSION['user_id'])) {
     include ("./components/signup_form.inc");
 }
 
-include ("./layouts/tail.inc"); // closing tags for layout div, body, and html
-
-include_once ("../db_connect.php"); // $msqli connect
-// $query = "SELECT * FROM users";
-// $result = mysqli_query($mysqli, $query);
-
-// if ($result) {
-//   $users = mysqli_fetch_all($result);
-
-//   foreach ($users as $user) {
-//     echo '<h6>Username: </h6>', $user[1], ' <h6>Registration date: </h6>', $user[3];
-//   }
-
-//   mysqli_free_result($result);
-// }
-mysqli_close($mysqli);
+include ("./layouts/tail.inc");
