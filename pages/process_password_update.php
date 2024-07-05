6ba58737153655763089c96264d47b0b120f66fd
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-04 22:53:44 -0500

    1

--- pages/process_password_update.php
+++ pages/process_password_update.php
@@ -22,8 +22,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (mysqli_stmt_execute($stmt)) {
         $successMessage = "Password updated successfully.";
 
-
-
     } else {
         $errorMessage = "Failed to update password: " . mysqli_stmt_error($stmt);
     }
