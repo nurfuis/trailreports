5369b6c7ba530771f2a6a77e7fe0d703f8ea0669
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-04 22:36:17 -0500

    1

--- pages/process_password_update.php
+++ pages/process_password_update.php
@@ -1,5 +1,4 @@
 <?php
-
 $page_title = "New Password";
 $page_css = "/assets/css/style.css";
 
@@ -12,24 +11,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $new_password = mysqli_real_escape_string($mysqli, trim($_POST['new_password']));
     $user_id = mysqli_real_escape_string($mysqli, trim($_POST['user_id']));
 
+    echo $user_id;
+
     $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 
-    $sql = "UPDATE users SET password_hash = ?, reset_token = NULL, reset_token_expiry = NULL WHERE user_id = ?";
+    $sql = "UPDATE users SET password_hash = ? WHERE user_id = ?";
 
     $stmt = mysqli_prepare($mysqli, $sql);
 
-    mysqli_stmt_bind_param($stmt, "sis", $hashed_password, $user_id);
+    mysqli_stmt_bind_param($stmt, "si", $hashed_password, $user_id);
 
     if (mysqli_stmt_execute($stmt)) {
         $successMessage = "Password updated successfully.";
-
-        // Reset reset_token and reset_token_expiry after successful update
-        $sql = "UPDATE users SET reset_token = NULL, reset_token_expiry = NULL WHERE user_id = ?";
-        $stmt = mysqli_prepare($mysqli, $sql);
-        mysqli_stmt_bind_param($stmt, "i", $user_id);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_close($stmt);
-
     } else {
         $errorMessage = "Failed to update password: " . mysqli_stmt_error($stmt);
     }
@@ -44,5 +37,3 @@ if (isset($errorMessage)) {
     echo '<p style="color: blue;">' . $successMessage . '</p>';
 }
 include ("../components/tail.inc");
-
-?>
