d278f8238ec4f4b6f79eaaf331f290d97d8053bc
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-04 22:06:25 -0500

    1

--- pages/process_email_login.php
+++ pages/process_email_login.php
@@ -71,7 +71,7 @@ mysqli_close($mysqli);
 
 if (!empty($errorMessage)) {
     echo "<p style='color: red;'>$errorMessage</p>";
-    include ("./components/sign_in_form.inc");
+    include ("../components/sign_in_form.inc");
 
 } else if (!empty($successMessage)) {
     echo "<p style='color: blue;'>$successMessage</p>";
