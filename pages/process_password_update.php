834efc0bdf4779d65a142556d078a86dd2d19531
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-04 22:58:32 -0500

    1

--- pages/process_password_update.php
+++ pages/process_password_update.php
@@ -11,17 +11,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $new_password = mysqli_real_escape_string($mysqli, trim($_POST['new_password']));
     $user_id = mysqli_real_escape_string($mysqli, trim($_POST['user_id']));
 
+    echo $user_id;
+
     $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 
     $sql = "UPDATE users SET password_hash = ? WHERE user_id = ?";
 
     $stmt = mysqli_prepare($mysqli, $sql);
 
-    mysqli_stmt_bind_param($stmt, "sis", $hashed_password, $user_id);
+    mysqli_stmt_bind_param($stmt, "si", $hashed_password, $user_id);
 
     if (mysqli_stmt_execute($stmt)) {
         $successMessage = "Password updated successfully.";
-        echo $successMessage;
+        header("Location: /pages/account.php");
     } else {
         $errorMessage = "Failed to update password: " . mysqli_stmt_error($stmt);
     }
