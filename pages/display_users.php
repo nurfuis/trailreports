90f8b71fa83e5d80291ff910ff6fd6464f0cafc5
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-10 13:29:24 -0500

    1

--- pages/display_users.php
+++ pages/display_users.php
@@ -6,7 +6,7 @@ $page_css = "../assets/css/style.css";
 
 // Include components
 include_once realpath("../components/head.inc"); // Top section up to and including body tag
-include_once realpath("../layouts/secondary.inc"); // An open div with layout class
+include_once realpath("../layouts/wide.inc"); // An open div with layout class
 include_once realpath("../../db_connect.php"); // $msqli connection
 
 // Build the user data query
