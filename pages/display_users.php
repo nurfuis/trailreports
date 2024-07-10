2d34830368ac4272ad3fb10702510f76bc4e1a97
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-10 16:12:36 -0500

    1

--- pages/display_users.php
+++ pages/display_users.php
@@ -7,7 +7,7 @@ include_once realpath("../components/head.inc"); // Top section up to and includ
 include_once realpath("../layouts/wide.inc"); // An open div with layout class
 require_once realpath("../../db_connect.php"); // $msqli connection
 
-$sql = "SELECT user_id, username, email, verified, account_status, last_login_attempt, registration_date 
+$sql = "SELECT user_id, username, email, verified, account_status, login_attempts, last_login_attempt, registration_date 
         FROM users 
         ORDER BY registration_date ASC";
 
@@ -18,7 +18,7 @@ if (!$result) {
 }
 
 echo "<table>";
-echo "<tr><th>ID</th><th>Username</th><th>Email</th><th>Verified</th><th>Registered</th><th>Last Login Attempt</th></tr>";
+echo "<tr><th>ID</th><th>Username</th><th>Email</th><th>Verified</th><th>Registered</th><th>Attempts</th><th>Last Login Attempt</th></tr>";
 
 while ($row = mysqli_fetch_assoc($result)) {
   echo "<tr>";
@@ -27,6 +27,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   echo "<td>" . $row["email"] . "</td>";
   echo "<td>" . ($row["verified"] ? "Yes" : "No") . "</td>"; // Convert verified to Yes/No
   echo "<td>" . $row["registration_date"] . "</td>";
+  echo "<td>" . $row["login_attempts"] . "</td>";
   echo "<td>" . $row["last_login_attempt"] . "</td>";
   echo "</tr>";
 }
