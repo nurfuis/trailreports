f23a85841f1290382bd106bda64055d6ee960ea9
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-10 16:10:20 -0500

    1

--- pages/process_sign_in.php
+++ pages/process_sign_in.php
@@ -13,74 +13,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = mysqli_real_escape_string($mysqli, trim($_POST['username']));
   $password = mysqli_real_escape_string($mysqli, trim($_POST['password']));
 
-  $sql = "SELECT user_id, username, password_hash, account_status FROM users WHERE username = ?";
+  $sql = "SELECT user_id, username, password_hash, account_status, login_attempts, last_login_attempt FROM users WHERE username = ?";
   $stmt = mysqli_prepare($mysqli, $sql);
   mysqli_stmt_bind_param($stmt, "s", $username);
   mysqli_stmt_execute($stmt);
-
   $result = mysqli_stmt_get_result($stmt);
-
   $num_rows = $result->num_rows;
+  $row = mysqli_fetch_assoc($result);
 
   if ($num_rows === 1) {
-    // Username exists
-    $row = mysqli_fetch_assoc($result);
+    // username exists
+    $attempts = $row['login_attempts'];
+    $lastAttempt = strtotime($row['last_login_attempt']);
+    $currentTime = time();
+    $threshold = 5;
+    $lockoutTime = 3600;
 
-    // record the last_login_attempt time
-    $sql_login = "UPDATE users SET last_login_attempt = NOW() WHERE user_id = ?";
-    $stmt_login = mysqli_prepare($mysqli, $sql_login);
-    mysqli_stmt_bind_param($stmt_login, "i", $row['user_id']);
-    mysqli_stmt_execute($stmt_login);
-    mysqli_stmt_close($stmt_login);
+    if ($attempts >= $threshold && ($currentTime - $lastAttempt) < $lockoutTime) {
+      // account is locked
+      $errorMessage = "Your account has been temporarily locked due to multiple failed login attempts. Please try again later.";
 
-    if (password_verify($password, $row['password_hash'])) {
-      // Login successful - Start session and store user data
-      session_start();
-      $_SESSION['username'] = $row['username'];
-      $_SESSION['user_id'] = $row['user_id'];
-      $_SESSION['authenticated'] = true;
-
-      if ($row['account_status'] === 'inactive') {
-        // Update account status to active
-        $sql = "UPDATE users SET account_status = 'active' WHERE user_id = ?";
-        $stmt = mysqli_prepare($mysqli, $sql);
-        mysqli_stmt_bind_param($stmt, "i", $row['user_id']);
-        mysqli_stmt_execute($stmt);
-
-        $successMessage = "Welcome back, " . $_SESSION['username'] . "! Your account has been activated.";
-      } else {
-        $successMessage = "Welcome back, " . $_SESSION['username'] . "!";
-      }
     } else {
-      $errorMessage = "Invalid username or password." . "\n CODE: 0";
+      // account is not locked
+      // check login
+      if (password_verify($password, $row['password_hash'])) {
+        // Login successful - Start session and store user data
+        session_start();
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['authenticated'] = true;
 
-      $checkLoginAttemptsSql = "SELECT login_attempts, last_login_attempt FROM users WHERE username = ?";
-      $checkLoginAttemptsStmt = mysqli_prepare($mysqli, $checkLoginAttemptsSql);
-      mysqli_stmt_bind_param($checkLoginAttemptsStmt, "s", $username);
-      mysqli_stmt_execute($checkLoginAttemptsStmt);
-      $result = mysqli_stmt_get_result($checkLoginAttemptsStmt);
-      $row = mysqli_fetch_assoc($result);
+        if ($row['account_status'] === 'inactive') {
+          // Update account status to active
+          $sql = "UPDATE users SET account_status = 'active' WHERE user_id = ?";
+          $stmt = mysqli_prepare($mysqli, $sql);
+          mysqli_stmt_bind_param($stmt, "i", $row['user_id']);
+          mysqli_stmt_execute($stmt);
 
-      if ($row) {
-        $errorMessage = "Invalid username or password." . "\n CODE: 2";
-        $attempts = $row['login_attempts'];
-        $lastAttempt = strtotime($row['last_login_attempt']);
-        $currentTime = time();
-        $threshold = 0;
-        $lockoutTime = 3600;
-
-        if ($attempts >= $threshold || ($currentTime - $lastAttempt) < $lockoutTime) {
-          $errorMessage = "Your account has been temporarily locked due to multiple failed login attempts. Please try again later.";
+          $successMessage = "Welcome back, " . $_SESSION['username'] . "! Your account has been activated.";
+        } else {
+          // account is active
+          $successMessage = "Welcome back, " . $_SESSION['username'] . "!";
         }
+      } else {
+        // failed login
+        // record the last_login_attempt time
+        $sql_login = "UPDATE users SET last_login_attempt = NOW() WHERE user_id = ?";
+        $stmt_login = mysqli_prepare($mysqli, $sql_login);
+        mysqli_stmt_bind_param($stmt_login, "i", $row['user_id']);
+        mysqli_stmt_execute($stmt_login);
+        mysqli_stmt_close($stmt_login);
 
+        $errorMessage = "Invalid username or password.";
       }
-
-
     }
   } else {
-    $errorMessage = "Invalid username or password." . "\n CODE: 1";
+    $errorMessage = "Invalid username or password.";
   }
-
   mysqli_free_result($result);
   mysqli_stmt_close($stmt);
 }
@@ -95,4 +84,4 @@ if (!empty($errorMessage)) {
   echo "<p style='color: blue;'>$successMessage</p>";
 }
 
-include realpath("../components/tail.inc"); // closing tags for layout div, body, and html 
+include realpath("../components/tail.inc");
