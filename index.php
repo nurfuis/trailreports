de9b67a194988fd9604631866e2cddbb420c42b7
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-10 15:17:48 -0500

    remove unused

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 $page_title = "Keep Sur Wild";
 $page_css = "./assets/css/style.css";
 
-include ("./components/head.inc");
+include_once realpath("./components/head.inc");
 
-include ("./layouts/main.inc");
+include_once realpath("./layouts/main.inc");
 
-include ("./components/nav.inc");
+include_once realpath("./components/nav.inc");
 
-include ("./components/header.inc");
+include_once realpath("./components/header.inc");
 
-include ("./components/topo_map.inc");
+include_once realpath("./components/topo_map.inc");
 
 session_start();
 if (isset($_SESSION['user_id'])) {
     $username = $_SESSION['username'];
-    include ("./components/sign_out_form.inc");
+    include_once realpath("./components/sign_out_form.inc");
 } else {
-    include ("./components/sign_in_form.inc");
+    include_once realpath("./components/sign_in_form.inc");
 }
 
-include ("./components/tail.inc");
+include_once realpath("./components/tail.inc");
