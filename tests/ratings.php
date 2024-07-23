04f08d5dc365257b595ce3daec4afbfd52cdd1f9
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 16:13:54 -0500

    1

--- tests/ratings.php
+++ tests/ratings.php
@@ -1,14 +1,14 @@
 <?php
 
-define('OVERALL_RATINGS', [
+$OVERALL_RATINGS = [
     'Good' => 1,
     'Passable' => 2,
     'Poor' => 3,
     'Impassable' => 4,
     'Gone' => 5
-]);
+];
 
-foreach (OVERALL_RATINGS as $rating_text => $rating_value): ?>
+foreach ($OVERALL_RATINGS as $rating_text => $rating_value): ?>
     <label class="k2d-regular" for="<?php echo $rating_value; ?>">
         <input type="radio" id="<?php echo $rating_value; ?>" name="rating" value="<?php echo $rating_value; ?>" required>
 
