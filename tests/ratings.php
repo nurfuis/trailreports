75345fa24f916d71bc136ac1f076e30db216dc38
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 16:06:58 -0500

    1

---
+++ tests/ratings.php
@@ -0,0 +1,13 @@
+<?php
+include_once realpath('../../config.php');
+echo OVERALL_RATINGS;
+
+foreach (OVERALL_RATINGS as $rating_text => $rating_value): ?>
+    <label class="k2d-regular" for="<?php echo $rating_value; ?>">
+        <input type="radio" id="<?php echo $rating_value; ?>" name="rating" value="<?php echo $rating_value; ?>" required>
+
+        <span><?php echo $rating_text; ?></span>
+    </label><br>
+
+
+<?php endforeach; ?>
