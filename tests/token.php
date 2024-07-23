f54011599ef6a9ba079ac722bdc04a23b967eecb
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 16:57:18 -0500

    1

--- tests/token.php
+++ tests/token.php
@@ -1,8 +1,8 @@
 <?php
 
-$tokenBytes = "1111";
-if ($tokenBytes !== false) {
-    echo '$tokenBytes';
+$token = md5(uniqid(mt_rand(), true));
+if ($token !== false) {
+    echo $token;
 } else {
     error_log("Failed to generate random bytes for token");
 }
