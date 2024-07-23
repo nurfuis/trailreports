b8b45c88cfd9dd74889b1efbbcba59919be51f08
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 16:54:46 -0500

    1

--- tests/token.php
+++ tests/token.php
@@ -2,7 +2,7 @@
 
 $tokenBytes = random_bytes(6);
 if ($tokenBytes !== false) {
-    $token = bin2hex($tokenBytes);
+    echo '$tokenBytes';
 } else {
     error_log("Failed to generate random bytes for token");
 }
