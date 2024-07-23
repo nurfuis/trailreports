154b421e1d327d942625ddf875821b4d4ba93332
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 15:41:17 -0500

    absolute link

--- pages/process_sign_up.php
+++ pages/process_sign_up.php
@@ -81,7 +81,7 @@ if (!empty($errorMessage)) {
 
 } else if (!empty($successMessage)) {
     echo "<p style='color: blue;'>$successMessage</p>";
-    header("Location: ./email.php");
+    header("Location: /pages/email.php");
 }
 
 
