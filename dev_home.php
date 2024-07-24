8b356c19bf7e1d4c96f246b25ecf0d3c8c43daa6
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 23:07:35 -0500

    1

--- dev_home.php
+++ dev_home.php
@@ -15,6 +15,20 @@ include_once realpath("./components/header.inc");
 include_once realpath("./components/intro.inc");
 
 include_once realpath("./components/dev_single_select_form.inc");
+if (isset($_GET['collection'])) {
+    $collection = $_GET['collection'];
+    $trail_sql = "SELECT id, name FROM features WHERE collections_id IN ($collection)";
+    $trail_result = mysqli_query($mysqli, $trail_sql);
+
+    if (!$trail_result) {
+        echo "Error: " . mysqli_error($mysqli);
+        exit;
+    }
+} else {
+    $trail_sql = "SELECT id, name FROM features WHERE collections_id IN (1)";
+    $trail_result = mysqli_query($mysqli, $trail_sql);
+}
+
 
 if (isset($_GET['feature_id'])) {
     $selected_feature_id = $_GET['feature_id'];
@@ -27,6 +41,8 @@ if (isset($_GET['feature_id'])) {
 } else {
     include_once realpath("./components/recent_reports.inc");
 }
+
+
 
 $mysqli->close();
 include_once realpath("./components/footer.inc");
