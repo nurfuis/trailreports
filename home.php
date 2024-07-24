a12122f1a0c7c67d4e4c59ad6d6452867806e54c
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-24 16:57:57 -0500

    1

--- home.php
+++ home.php
@@ -1,24 +1,21 @@
 <?php
-$page_title = "Trail Reports for Big Sur, California";
-$page_css = "./assets/css/style.css";
-$currentPagePath = $_SERVER['SCRIPT_NAME'];
-
 session_start();
-
-// Remember to update config and db_connect on the deployment server
-require_once realpath("../config.php");
 require_once realpath("../db_connect.php");
-
-include_once realpath("./components/head.inc");
-include_once realpath("./layouts/main.inc");
-include_once realpath("./components/header.inc");
-include_once realpath("./components/intro.inc");
 
 if (isset($_GET['collection'])) {
     $collection = $_GET['collection'];
 } else {
     $collection = 1;
 }
+
+$page_title = "Trail Reports for Big Sur, California";
+$page_css = "./assets/css/style.css";
+$currentPagePath = $_SERVER['HTTP_REFERER'];
+
+include_once realpath("./components/head.inc");
+include_once realpath("./layouts/main.inc");
+include_once realpath("./components/header.inc");
+include_once realpath("./components/intro.inc");
 include_once realpath("./components/single_select_form.inc");
 
 if (isset($_GET['feature_id'])) {
