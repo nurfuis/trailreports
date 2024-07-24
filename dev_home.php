ae2415e5f857a71a7c7e8ad8e31eef6a0c2bf86a
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-23 23:20:15 -0500

    1

--- dev_home.php
+++ dev_home.php
@@ -14,26 +14,12 @@ include_once realpath("./layouts/main.inc");
 include_once realpath("./components/header.inc");
 include_once realpath("./components/intro.inc");
 
-include_once realpath("./components/dev_single_select_form.inc");
-if (isset($_GET['collection'])) {
+if (isset($_GET['feature_id'])) {
     $collection = $_GET['collection'];
-    $trail_sql = "SELECT id, name FROM features WHERE collections_id IN ($collection)";
-    $trail_result = mysqli_query($mysqli, $trail_sql);
-
-    if (!$trail_result) {
-        echo "Error: " . mysqli_error($mysqli);
-        exit;
-    }
 } else {
-    $trail_sql = "SELECT id, name FROM features WHERE collections_id IN (1)";
-    $trail_result = mysqli_query($mysqli, $trail_sql);
-
-    if (!$trail_result) {
-        echo "Error: " . mysqli_error($mysqli);
-        exit;
-    }
-
+    $collection = 1;
 }
+include_once realpath("./components/dev_single_select_form.inc");
 
 if (isset($_GET['feature_id'])) {
     $selected_feature_id = $_GET['feature_id'];
@@ -46,8 +32,6 @@ if (isset($_GET['feature_id'])) {
 } else {
     include_once realpath("./components/recent_reports.inc");
 }
-
-
 
 $mysqli->close();
 include_once realpath("./components/footer.inc");
