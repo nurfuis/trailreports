bb2df261f865948f5667405e77898983d0da3235
nurfuis <cramer.ross@gmail.com>
2024-08-06 20:21:51 -0500

    1

--- pages/hide_report.php
+++ pages/hide_report.php
@@ -1,21 +1,26 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_level']) || $_SESSION['user_level'] !== 'admin') {
-    // Redirect if user is not admin
-    header("Location: /home.php");
-    exit;
-}
 require_once realpath("../../db_connect.php");
+require_once realpath("../components/user_roles.php");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
 $reportId = isset($_POST['report_id']) ? intval($_POST['report_id']) : 0;
+$user_id = isset($_POST['user_id']) ? intval($_POST['user_id']) : 0;
+$logged_in_id = get_user_id();
 
 if (!$reportId) {
     die("Invalid report ID");
 }
+
+if ($logged_in_id != $user_id) {
+    if (!is_admin()) {
+        exit;
+    }
+}
+
 
 $sql = "UPDATE trail_reports SET active = 0 WHERE id = ?";
 $stmt = mysqli_prepare($mysqli, $sql);
@@ -24,13 +29,12 @@ mysqli_stmt_bind_param($stmt, "i", $reportId);
 if (mysqli_stmt_execute($stmt)) {
     mysqli_stmt_close($stmt);
     mysqli_close($mysqli);
-    ?>
+?>
     <script type="text/javascript">
-        window.location.href = "/home.php" 
+        window.location.href = "/home.php"
     </script>
 
-    <?php
-    // header("Location: hidden_reports.php");
+<?php
     exit;
 } else {
     echo "Error hiding report: " . mysqli_error($mysqli);
