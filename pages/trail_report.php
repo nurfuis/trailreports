3818de99f2821d8ecffad5034ec1dee5d4496a18
nurfuis <cramer.ross@gmail.com>
2024-08-08 20:11:23 -0500

    1

--- pages/trail_report.php
+++ pages/trail_report.php
@@ -7,14 +7,14 @@ $previousPage = $_SERVER['HTTP_REFERER'];
 $page_title = "Trail Report";
 $stylesheet = "../assets/css/style.css";
 
-$reportId = isset($_GET['id']) ? intval($_GET['id']) : 0;
+$selected_feature_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
 $sql = "SELECT tr.*, f.name AS feature_name, 
        COALESCE(tr.title, 'Untitled') AS report_title, u.username 
        FROM trail_reports tr 
        INNER JOIN features f ON tr.feature_id = f.id 
        INNER JOIN users u ON tr.user_id = u.user_id 
-       WHERE tr.id = $reportId AND active = 1;";
+       WHERE tr.id = $selected_feature_id AND active = 1;";
 
 $result = $mysqli->query($sql);
 if (!$result) {
