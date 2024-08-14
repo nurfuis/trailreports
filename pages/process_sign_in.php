19b50640808142572b2dd127751a58c1998acb09
nurfuis <cramer.ross@gmail.com>
2024-08-14 14:48:10 -0500

    1

--- pages/process_sign_in.php
+++ pages/process_sign_in.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lastAttempt = strtotime($row['last_login_attempt']);
     $currentTime = time();
     $threshold = 100;
-    $lockoutTime = 20000;
+    $lockoutTime = 1;
 
     if ($attempts >= $threshold && ($currentTime - $lastAttempt) < $lockoutTime) {
       // account is locked
