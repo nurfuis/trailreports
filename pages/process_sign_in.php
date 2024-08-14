79867c8034763b5abe08dedac9f0b52dd6ac293b
nurfuis <cramer.ross@gmail.com>
2024-08-14 14:45:37 -0500

    1

--- pages/process_sign_in.php
+++ pages/process_sign_in.php
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $attempts = $row['login_attempts'];
     $lastAttempt = strtotime($row['last_login_attempt']);
     $currentTime = time();
-    $threshold = 50;
+    $threshold = 100;
     $lockoutTime = 20000;
 
     if ($attempts >= $threshold && ($currentTime - $lastAttempt) < $lockoutTime) {
